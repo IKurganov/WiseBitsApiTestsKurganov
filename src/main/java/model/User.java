b09e55e9bd3ba8deea6998794b4fb40180d4828b
KEmma6 <emma.chuyako@gmail.com>
2022-05-12 01:44:49 +0300

    * - second test is done

---
+++ src/main/java/model/User.java
@@ -0,0 +1,67 @@
+package model;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class User {
+        private Long id;
+        private String username;
+        private String email;
+        private String password;
+        private String created_at;
+        private String updated_at;
+
+
+        public Long getId() {
+                return id;
+        }
+
+        public void setId(Long id) {
+                this.id = id;
+        }
+
+        public String getUsername() {
+                return username;
+        }
+
+        public void setUsername(String username) {
+                this.username = username;
+        }
+
+        public String getEmail() {
+                return email;
+        }
+
+        public void setEmail(String email) {
+                this.email = email;
+        }
+
+        public String getPassword() {
+                return password;
+        }
+
+        public void setPassword(String password) {
+                this.password = password;
+        }
+
+        public String getCreated_at() {
+                return created_at;
+        }
+
+        public void setCreated_at(String created_at) {
+                this.created_at = created_at;
+        }
+
+        public String getUpdated_at() {
+                return updated_at;
+        }
+
+        public void setUpdated_at(String updated_at) {
+                this.updated_at = updated_at;
+        }
+
+        public String toString() {
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                return gson.toJson(this);
+        }
+}
--- src/test/java/PaymenticoTests.java
+++ src/test/java/PaymenticoTests.java
@@ -1,5 +1,8 @@
 import com.github.javafaker.Faker;
+import model.User;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
@@ -35,5 +38,28 @@ public class PaymenticoTests {
                 .log().all()
                 .statusCode(200)
                 .body(notNullValue());
+
+//        + к кейсам можно добавить проверку на идемпотентность метода GET
+//        List<User> users = specs.getUsers()
+//                .then()
+//                .log().all()
+//                .extract().body().jsonPath().getList("$", User.class);
+//
+    }
+
+    @Test
+    public void checkUniquenessOfUsersRegistration() {
+
+        String nonUniqueUsername = new Faker().name().username();
+        String nonUniqueEmail = new Faker().internet().emailAddress();
+        String password = new Faker().internet().password();
+
+        specs.createUser(username, email, password)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("message", is("User Successully created"))  // текст взят из ответа
+                .body("details.username", is(username))
+                .body("details.email", is(email));
     }
 }
